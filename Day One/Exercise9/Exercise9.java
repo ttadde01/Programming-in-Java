d2e91648e9194c71ae3fdbbcf5a767bd95d6c512
ttadde01 <00addis@live.co.uk>
2014-10-20 19:07:05 +0100

    Update Exercise9.java

--- Day One/Exercise9/Exercise9.java
+++ Day One/Exercise9/Exercise9.java
@@ -1,11 +1,12 @@
 /******************************************************************************************************
 * Day One: Exercise 9                                                                                 *
 * Read an arbitrarily long sequence of positive numbers from the user, until -1                       *
-* is entered. At that point, print ”Yes” if the numbers were consecutive and                          *
-* increasing and ”No” otherwise. Sequences ”1,2,3,4,-1” and ”5,6,7,8,9,10,11,-                        *
-* 1” should output ”Yes”, but ”2,3,5,6,7,-1”, ”10,9,8,7,-1”, and ”1,1,2,3,4,5,-1” should output ”No”. *                                                                                       *
+* is entered. At that point, print â€Yesâ€ if the numbers were consecutive and                          *
+* increasing and â€Noâ€ otherwise. Sequences â€1,2,3,4,-1â€ and â€5,6,7,8,9,10,11,-                        *
+* 1â€ should output â€Yesâ€, but â€2,3,5,6,7,-1â€, â€10,9,8,7,-1â€, and â€1,1,2,3,4,5,-1â€ should output â€Noâ€.
+ *                                                                                       *
 *                                                                                                     *
-* Author: Tadiwos Taddese                                                                             *
+* Author: ttadde01                                                                             *
 *******************************************************************************************************/
 import java.util.Scanner;
 import java.util.Arrays;
